b562ec9d51d970055cbe703444250dd5738f6821
Jacob Marks <jamarks13@gmail.com>
2023-08-07 14:11:59 -0700

    Linting

--- plugins/txt2img/__init__.py
+++ plugins/txt2img/__init__.py
@@ -15,12 +15,37 @@ import fiftyone as fo
 import fiftyone.core.utils as fou
 
 import requests
+
 openai = fou.lazy_import("openai")
 replicate = fou.lazy_import("replicate")
 
 SD_MODEL_URL = "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478"
-SD_SCHEDULER_CHOICES = ("DDIM", "K_EULER", "DPMSolverMultiStep", "K_EULER_ANCESTRAL", "PNDM", "KLMS")
-SD_SIZE_CHOICES = ("64", "128", "192", "256", "320", "384", "448", "512", "576", "640", "704", "768", "832", "896", "960", "1024")
+SD_SCHEDULER_CHOICES = (
+    "DDIM",
+    "K_EULER",
+    "DPMSolverMultiStep",
+    "K_EULER_ANCESTRAL",
+    "PNDM",
+    "KLMS",
+)
+SD_SIZE_CHOICES = (
+    "64",
+    "128",
+    "192",
+    "256",
+    "320",
+    "384",
+    "448",
+    "512",
+    "576",
+    "640",
+    "704",
+    "768",
+    "832",
+    "896",
+    "960",
+    "1024",
+)
 
 VQGAN_MODEL_URL = "mehdidc/feed_forward_vqgan_clip:28b5242dadb5503688e17738aaee48f5f7f5c0b6e56493d7cf55f74d02f144d8"
 
@@ -29,22 +54,26 @@ DALLE2_SIZE_CHOICES = ("256x256", "512x512", "1024x1024")
 
 def allows_replicate_models():
     """Returns whether the current environment allows replicate models."""
-    return find_spec("replicate") is not None and 'REPLICATE_API_TOKEN' in os.environ
+    return (
+        find_spec("replicate") is not None
+        and "REPLICATE_API_TOKEN" in os.environ
+    )
 
 
 def allows_openai_models():
     """Returns whether the current environment allows openai models."""
-    return find_spec("openai") is not None and 'OPENAI_API_KEY' in os.environ
+    return find_spec("openai") is not None and "OPENAI_API_KEY" in os.environ
 
 
 def download_image(image_url, filename):
     img_data = requests.get(image_url).content
-    with open(filename, 'wb') as handler:
+    with open(filename, "wb") as handler:
         handler.write(img_data)
 
 
-class Text2Image():
+class Text2Image:
     """Wrapper for a Text2Image model."""
+
     def __init__(self):
         self.name = None
         self.model_name = None
@@ -55,10 +84,11 @@ class Text2Image():
 
 class StableDiffusion(Text2Image):
     """Wrapper for a StableDiffusion model."""
+
     def __init__(self):
         super().__init__()
         self.name = "stable-diffusion"
-        self.model_name =SD_MODEL_URL
+        self.model_name = SD_MODEL_URL
 
     def generate_image(self, ctx):
         prompt = ctx.params.get("prompt", "None provided")
@@ -67,7 +97,16 @@ class StableDiffusion(Text2Image):
         inference_steps = ctx.params.get("inference_steps", "None provided")
         scheduler = ctx.params.get("scheduler_choices", "None provided")
 
-        response = replicate.run(self.model_name, input={"prompt": prompt, "width": width, "height": height, "inference_steps": inference_steps, "scheduler": scheduler})
+        response = replicate.run(
+            self.model_name,
+            input={
+                "prompt": prompt,
+                "width": width,
+                "height": height,
+                "inference_steps": inference_steps,
+                "scheduler": scheduler,
+            },
+        )
         if type(response) == list:
             response = response[0]
         return response
@@ -75,6 +114,7 @@ class StableDiffusion(Text2Image):
 
 class DALLE2(Text2Image):
     """Wrapper for a DALL-E 2 model."""
+
     def __init__(self):
         super().__init__()
         self.name = "dalle-2"
@@ -83,15 +123,13 @@ class DALLE2(Text2Image):
         prompt = ctx.params.get("prompt", "None provided")
         size = ctx.params.get("size_choices", "None provided")
 
-        response = openai.Image.create(
-            prompt=prompt,
-            n=1,
-            size=size
-        )
-        return response['data'][0]['url']
+        response = openai.Image.create(prompt=prompt, n=1, size=size)
+        return response["data"][0]["url"]
+
 
 class VQGANCLIP(Text2Image):
     """Wrapper for a VQGAN-CLIP model."""
+
     def __init__(self):
         super().__init__()
         self.name = "vqgan-clip"
@@ -112,11 +150,8 @@ def get_model(model_name):
         return DALLE2()
     if model_name == "vqgan-clip":
         return VQGANCLIP()
-    
+
     raise ValueError(f"Model {model_name} not found.")
-
-
-
 
 
 def generate_filepath(dataset):
@@ -144,9 +179,12 @@ class Txt2Image(foo.Operator):
         replicate_flag = allows_replicate_models()
         openai_flag = allows_openai_models()
         if not replicate_flag and not openai_flag:
-            inputs.message("message", label="No models available. Please set up your environment variables.")
+            inputs.message(
+                "message",
+                label="No models available. Please set up your environment variables.",
+            )
             return types.Property(inputs)
-        
+
         radio_choices = types.RadioGroup()
         if replicate_flag:
             radio_choices.add_choice("sd", label="Stable Diffusion")
@@ -160,7 +198,6 @@ class Txt2Image(foo.Operator):
             label="Models",
             view=radio_choices,
         )
-
 
         if ctx.params.get("model_choices", False) == "sd":
             size_choices = SD_SIZE_CHOICES
@@ -188,16 +225,17 @@ class Txt2Image(foo.Operator):
 
             inference_steps_slider = types.SliderView(
                 label="Num Inference Steps",
-                componentsProps={"slider": {"min": 1, "max": 500, "step": 1}}
+                componentsProps={"slider": {"min": 1, "max": 500, "step": 1}},
             )
             inputs.int(
-                "inference_steps",
-                default = 50,
-                view=inference_steps_slider)
+                "inference_steps", default=50, view=inference_steps_slider
+            )
 
             scheduler_choices_dropdown = types.Dropdown(label="Scheduler")
             for scheduler in SD_SCHEDULER_CHOICES:
-                scheduler_choices_dropdown.add_choice(scheduler, label=scheduler)
+                scheduler_choices_dropdown.add_choice(
+                    scheduler, label=scheduler
+                )
 
             inputs.enum(
                 "scheduler_choices",
@@ -219,7 +257,6 @@ class Txt2Image(foo.Operator):
                 view=size_choices_dropdown,
             )
 
-
         inputs.str("prompt", label="Prompt", required=True)
         return types.Property(inputs)
 
@@ -235,16 +272,17 @@ class Txt2Image(foo.Operator):
         sample = fo.Sample(
             filepath=filename,
             tags=["generated"],
-            model = model.name,
-            prompt = prompt,
+            model=model.name,
+            prompt=prompt,
         )
         ctx.dataset.add_sample(sample)
         ctx.trigger("reload_samples")
 
-
     def resolve_output(self, ctx):
         outputs = types.Object()
-        outputs.str("prompt", label="Successfully generated image with prompt:")
+        outputs.str(
+            "prompt", label="Successfully generated image with prompt:"
+        )
         return types.Property(outputs)
 
 
