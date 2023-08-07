64d61ca317b2b2a8326f8d2de38503b71c4dfda4
Jacob Marks <jamarks13@gmail.com>
2023-08-07 13:37:19 -0700

    cite equation

--- plugins/common_issues/__init__.py
+++ plugins/common_issues/__init__.py
@@ -27,6 +27,11 @@ def compute_sample_brightness(sample):
             stat.mean[0],
             stat.mean[0],
         )
+
+    ## equation from here:
+    ## https://www.nbdtech.com/Blog/archive/2008/04/27/calculating-the-perceived-brightness-of-a-color.aspx
+    ## and here:
+    ## https://github.com/cleanlab/cleanvision/blob/72a1535019fe7b4636d43a9ef4e8e0060b8d66ec/src/cleanvision/issue_managers/image_property.py#L95
     brightness = np.sqrt(0.241 * r ** 2 + 0.691 * g ** 2 + 0.068 * b ** 2)/255
     return brightness
 
