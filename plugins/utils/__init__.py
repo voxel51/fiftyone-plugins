cddedeae03aa85219e302d07305e022853541f7d
brimoor <brimoor@umich.edu>
2024-02-19 10:57:12 -0800

    allow operator to be triggered manually

--- plugins/utils/__init__.py
+++ plugins/utils/__init__.py
@@ -1993,7 +1993,6 @@ class LoadDefaultView(foo.Operator):
             name="load_default_view",
             label="Load default view",
             on_startup=True,
-            unlisted=True,
         )
 
     def execute(self, ctx):
