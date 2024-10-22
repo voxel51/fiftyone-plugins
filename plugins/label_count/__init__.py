cfc79b56fdb3a628eac110f5f230b2e3ecbd6a07
brimoor <brimoor@umich.edu>
2024-10-22 00:43:50 -0400

    add support for dynamic groups in video navigation mode

--- plugins/label_count/__init__.py
+++ plugins/label_count/__init__.py
@@ -1,11 +1,16 @@
-from bson import ObjectId
+"""
+Label count operators.
 
+| Copyright 2017-2024, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
 import fiftyone as fo
 import fiftyone.operators as foo
 import fiftyone.operators.types as types
-from fiftyone import ViewField as F
 import fiftyone.core.media as fom
 import fiftyone.core.view as fov
+from fiftyone import ViewField as F
 
 
 class LabelCountPanel(foo.Panel):
@@ -13,77 +18,46 @@ class LabelCountPanel(foo.Panel):
     def config(self):
         return foo.PanelConfig(
             name="label_count_panel",
-            label="Label Count",
+            label="Label count",
             surfaces="modal",
             allow_multiple=True,
             reload_on_navigation=True,
         )
 
-    def _iter_frames(self, ctx):
-        current_sample = fov.make_optimized_select_view(
-            ctx.view, ctx.current_sample
-        ).first()
-        if ctx.view.media_type == fom.VIDEO:
-            for frame in current_sample.frames.values():
-                yield frame
-
-            return
-
-        if (
-            ctx.view.media_type == fom.GROUP
-            and ctx.view._parent_media_type == fom.IMAGE
-        ):
-            for frame in ctx.dataset.match(
-                F("_sample_id") == ObjectId(current_sample.sample_id)
-            ):
-                yield frame
-
-            return
-
-        raise ValueError("unexpected")
-
     def on_load(self, ctx):
-        if not ctx.current_sample:
-            ctx.panel.state.empty_state = (
-                "Please select a valid numeric or list field."
-            )
-            return
-
-        # Extract the selected field and its values
-        sample_id = ctx.current_sample
         field = ctx.panel.state.selected_field
         if not field:
             ctx.panel.state.empty_state = (
-                "Please select a valid field from the dropdown."
+                "Please select a valid numeric or list field"
             )
             return
 
-        # Get frame values for the selected field
-        frames = list(self._iter_frames(ctx))
-        frame_numbers, values = _get_frame_values(
-            ctx.dataset, sample_id, field
-        )
+        if ctx.view._is_dynamic_groups:
+            sample_id = ctx.view[ctx.current_sample].sample_id
+        else:
+            sample_id = ctx.current_sample
 
-        # Set the plot state for rendering the bar plot
+        frame_numbers, values = _get_values(ctx.view, sample_id, field)
+
         ctx.panel.state.plot = {
             "type": "bar",
             "x": frame_numbers,
             "y": values,
             "title": field,
-            "xaxis": "Frame Number",
+            "xaxis": "Frame number",
             "yaxis": field,
             "hoverinfo": "x+y",
             "marker": {"color": ctx.panel.state.plot_color or "#ff6d04"},
             "selectedpoints": [],
             "hoverinfo": "x+y",
             "hovertemplate": (
-                f"<b>Frame Number</b>: %{'{x}'}<br><b>{ctx.panel.state.selected_field}</b>: %{'{y}'}<extra></extra>"
+                f"<b>Frame number</b>: %{'{x}'}<br><b>{ctx.panel.state.selected_field}</b>: %{'{y}'}<extra></extra>"
             ),
         }
-        self.load_range(ctx, (0, len(frames)))
+        self.load_range(ctx, (0, len(frame_numbers)))
 
     def render(self, ctx):
-        valid_fields = _get_frame_paths(ctx.dataset)
+        valid_fields = _get_fields(ctx.view)
         if not ctx.panel.state.selected_field:
             empty_state = types.Object()
             empty_state.enum(
@@ -120,7 +94,7 @@ class LabelCountPanel(foo.Panel):
                 menu.enum(
                     "field_selector",
                     valid_fields,
-                    label="Select a Field",
+                    label="Select a field",
                     default=selected_field,
                     on_change=self.on_field_select,
                 )
@@ -130,12 +104,12 @@ class LabelCountPanel(foo.Panel):
                     layout={
                         "title": {"text": selected_field},
                         "margin": {"t": 30},
-                        "xaxis": {"title": "Frame Number"},
+                        "xaxis": {"title": "Frame number"},
                         "yaxis": {"title": selected_field},
                     },
                 )
         else:
-            panel.md("No valid fields found in the dataset.")
+            panel.md("There are no compatible fields to display")
 
         return types.Property(
             panel, view=types.GridView(height=100, width=100)
@@ -144,7 +118,7 @@ class LabelCountPanel(foo.Panel):
     def on_field_select(self, ctx):
         field_name = ctx.params.get("value")
         ctx.panel.state.selected_field = field_name
-        ctx.panel.set_title(f"{field_name} Plot")
+        ctx.panel.set_title(f"{field_name} plot")
 
         self.on_load(ctx)
 
@@ -171,26 +145,40 @@ class LabelCountPanel(foo.Panel):
         return [frame - 1]
 
 
-def _get_frame_values(dataset, sample_id, path):
-    view = dataset.select(sample_id)
-    field = dataset.get_field("frames." + path)
-    expr = "frames[]." + path
-    if isinstance(field, fo.ListField):
-        expr = F(expr).length()
-    return view.values(["frames[].frame_number", expr])
+def _get_fields(view):
+    if view._is_dynamic_groups:
+        schema_fcn = view.get_field_schema
+    else:
+        schema_fcn = view.get_frame_field_schema
 
-
-def _get_frame_paths(dataset):
-    schema = dataset.get_frame_field_schema(
+    schema = schema_fcn(
         flat=True, ftype=(fo.FloatField, fo.IntField, fo.ListField)
     )
-    if schema:
-        return [
-            path
-            for path in schema
-            if not any(path.startswith(p + ".") for p in schema)
-        ]
-    return []
+    if not schema:
+        return []
+
+    return [
+        path
+        for path in schema
+        if not any(path.startswith(p + ".") for p in schema)
+    ]
+
+
+def _get_values(view, sample_id, path):
+    if view._is_dynamic_groups:
+        field = view.get_field(path)
+        expr = path
+        if isinstance(field, fo.ListField):
+            expr = F(expr).length()
+        view = view.get_dynamic_group(sample_id)
+        return view.values(["frame_number", expr])
+    else:
+        field = view.get_field("frames." + path)
+        expr = "frames[]." + path
+        if isinstance(field, fo.ListField):
+            expr = F(expr).length()
+        view = fov.make_optimized_select_view(view, [sample_id])
+        return view.values(["frames[].frame_number", expr])
 
 
 def register(p):
